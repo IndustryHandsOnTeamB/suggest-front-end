a65f3a469e95ff7cd03c472ad7bf483d93f1793a
ilene97 <ilene97@naver.com>
2020-11-29 17:18:50 +0900

    add json

--- HandsOn/app/src/main/java/com/example/handson/LogInActivity.java
+++ HandsOn/app/src/main/java/com/example/handson/LogInActivity.java
@@ -3,10 +3,22 @@ package com.example.handson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -26,9 +38,10 @@ public class LogInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String id = userId.getText().toString();
                 String pwd = userPwd.getText().toString();
+                SignInJson signIn = new SignInJson();
+                signIn.execute(id, pwd);
                 Intent intent = new Intent(getApplicationContext(), MenuSelect.class);
                 intent.putExtra("userId", id);
-                intent.putExtra("userPwd", pwd);
                 finish();
                 startActivity(intent);
             }
@@ -42,4 +55,96 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
     }
+
+    public class SignInJson extends AsyncTask<String, Void, String> {
+        private int statusCode;
+        public String doInBackground(String... params) {
+            String id = params[0];
+            String pw = params[1];
+            String email = params[2];
+            String name = params[3];
+
+            try {
+                // 로그인 시 입력한 정보를 Json object로 만들어서
+                JSONObject myJsonObject = new JSONObject();
+                try {
+                    myJsonObject.put("login_id", id);
+                    myJsonObject.put("name", pw);
+                    myJsonObject.put("pwd", email);
+                    myJsonObject.put("email",name);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // 서버 api에 전송을 시도한다
+                URL obj = new URL("http://ec2-3-34-135-151.ap-northeast-2.compute.amazonaws.com/admin/");
+
+                HttpURLConnection conn = (HttpURLConnection) obj.openConnection(); // open connection
+
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                // 데이터를 읽어올 것이고
+                conn.setDoInput(true);
+                // 데이터를 쓸 것이다.
+                conn.setDoOutput(true);
+
+                // property 지정해주고
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Content-Type", "application/json");
+
+                // 전송을 해본다
+                OutputStream os = conn.getOutputStream();
+                os.write(myJsonObject.toString().getBytes());
+                os.flush();
+                os.close();
+
+
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+                String line;
+                StringBuilder sb = new StringBuilder();
+
+                statusCode = conn.getResponseCode();
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+                return sb.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (s == null) {
+                // 서버에서 널 값이 온경우. API가 이상하거나. 서버가 꺼져있는 경우
+            } else {
+                try {
+                    // 수신한 data s에 대해
+                    JSONObject jsonObject = new JSONObject(s);
+
+                    if(statusCode == 201){
+                        // 데이터들을 추출하여 변수에 저장한다.
+                        //String statusFromServer = jsonObject.get("status").toString();
+
+                    }
+
+                } catch (JSONException e) {
+
+                }
+            }
+        }
+
+    }
 }
+
+
--- HandsOn/app/src/main/java/com/example/handson/SignUpActivity.java
+++ HandsOn/app/src/main/java/com/example/handson/SignUpActivity.java
@@ -1,7 +1,8 @@
 package com.example.handson;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,13 +13,22 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        final boolean usableId = false;
+        final boolean usableId = true;
         final String[] userTypeList = {"중학생","고등학생","대학생","기타"};
 
         final Spinner userTypeSpinner = (Spinner)findViewById(R.id.user_type_spinner);
@@ -31,6 +41,7 @@ public class SignUpActivity extends AppCompatActivity {
         Button cancelButton = (Button)findViewById(R.id.cancel_button);
 
         final EditText userName = (EditText)findViewById(R.id.user_name);
+        final EditText userEmail = (EditText)findViewById(R.id.email);
         final EditText userId = (EditText)findViewById(R.id.user_id);
         final EditText userPwd = (EditText)findViewById(R.id.user_pwd);
         final EditText userPwdCheck = (EditText)findViewById(R.id.user_pwd_check);
@@ -40,12 +51,12 @@ public class SignUpActivity extends AppCompatActivity {
         idCheckButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newId = userId.getText().toString();
-                //같은 아이디 있는지 check
-                if(usableId)
-                    Toast.makeText(getApplicationContext(),"사용가능한 아이디입니다.",Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getApplicationContext(),"이미 사용중인 아이디입니다.",Toast.LENGTH_SHORT).show();
+                //String newId = userId.getText().toString();
+                ////같은 아이디 있는지 check
+                //if(usableId)
+                //    Toast.makeText(getApplicationContext(),"사용가능한 아이디입니다.",Toast.LENGTH_SHORT).show();
+                //else
+                //    Toast.makeText(getApplicationContext(),"이미 사용중인 아이디입니다.",Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -68,19 +79,22 @@ public class SignUpActivity extends AppCompatActivity {
                     userInfo[0] = userName.getText().toString();
                     userInfo[1] = userId.getText().toString();
                     userInfo[2] = userPwd.getText().toString();
+                    userInfo[3] = userEmail.getText().toString();
                     userTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
                         @Override
                         public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id){
-                            userInfo[3] = adapterView.getItemAtPosition(position).toString();
+                            userInfo[4] = adapterView.getItemAtPosition(position).toString();
                         }
                         @Override
                         public void onNothingSelected(AdapterView<?> adapterView){
 
                         }
                     });
-                    //Intent intent = new Intent(getApplicationContext(), nextActivity.class);
-                    //intent.putExtra("userInfo", userInfo);
-                    //startActivity(intent);
+
+                    SignUpJson signIn = new SignUpJson();
+                    signIn.execute(userInfo[1], userInfo[3],userInfo[0],userInfo[2]);       // id, email, name, password
+
+                    finish();
                 }
             }
         });
@@ -91,6 +105,100 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+
+    public class SignUpJson extends AsyncTask<String, Void, String> {
+        private int statusCode;
+        public String doInBackground(String... params) {
+            String id = params[0];
+            String email = params[1];
+            String name = params[2];
+            String password = params[3];
+            Log.d("json","parameters "+id);
+
+            try {
+                // 로그인 시 입력한 정보를 Json object로 만들어서
+                JSONObject myJsonObject = new JSONObject();
+                try {
+                    myJsonObject.put("username", id);
+                    myJsonObject.put("email", email);
+                    myJsonObject.put("name",name);
+                    myJsonObject.put("input_password", password);
+                    Log.d("json","json object 추가");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // 서버 api에 전송을 시도한다
+                URL obj = new URL("http://ec2-3-34-135-151.ap-northeast-2.compute.amazonaws.com/api/v1/users/signup/");
+                HttpURLConnection conn = (HttpURLConnection) obj.openConnection(); // open connection
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                // 데이터를 읽어올 것이고
+                conn.setDoInput(true);
+                // 데이터를 쓸 것이다.
+                conn.setDoOutput(true);
+
+                // property 지정해주고
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Content-Type", "application/json");
+                Log.d("json","------------------------------여기까진 됨-----------------------------");
+                // 전송을 해본다
+                OutputStream os = conn.getOutputStream();
+                os.write(myJsonObject.toString().getBytes());
+                os.flush();
+                os.close();
+
+                Log.d("json","------------------------------json 전송-----------------------------");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+                String line;
+                StringBuilder sb = new StringBuilder();
+
+                statusCode = conn.getResponseCode();
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+                return sb.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("json","on post execute");
+
+            if (s == null) {
+                // 서버에서 널 값이 온경우. API가 이상하거나. 서버가 꺼져있는 경우
+                Log.d("json","null from server");
+            } else {
+                try {
+                    // 수신한 data s에 대해
+                    JSONObject jsonObject = new JSONObject(s);
+                    Log.d("json","오긴 왓음");
+
+                    if(statusCode == 201){
+                        // 데이터들을 추출하여 변수에 저장한다.
+                        Log.d("json","제대로 왔음");
+                        String idFromServer = jsonObject.get("username").toString();
+                        Toast.makeText(getApplicationContext(),idFromServer + " 회원가입 성공",Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+
+                }
+            }
+        }
 
     }
 }
